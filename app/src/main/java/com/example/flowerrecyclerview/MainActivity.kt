094c103205226382372a0053a60bd1bfed07caaa
graceaudryw <wicaksana.grace10@gmail.com>
2019-10-20 13:58:18 +0700

    first commit

---
+++ app/src/main/java/com/example/flowerrecyclerview/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.flowerrecycleview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.flowerrecyclerview.Item
+import com.example.flowerrecyclerview.RecyclerViewAdapter
+
+class MainActivity : AppCompatActivity() {
+
+    private val items: MutableList<Item> = mutableListOf()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main
+            R.layout.activity_main)
+
+        val list = findViewById<RecyclerView>(R.id.flower_list)
+        initData()
+
+        list.layoutManager = LinearLayoutManager(this)
+        list.adapter = RecyclerViewAdapter(this,items)
+
+    }
+
+    private fun initData(){
+        val name = resources.getStringArray(R.array.flower_name)
+        val image = resources.obtainTypedArray(R.array.flower_image)
+
+        items.clear()
+
+        for (i in name.indices){
+            items.add(Item(name[i],image.getResourceId(i,0)))
+        }
+
+        image.recycle()
+    }
+}
